34c2866f2cb338e831b35b752092544b73d8ceda
kaankvrck <kaankvrck@gmail.com>
2020-09-24 07:32:17 +0300

    Exchange Api Project EcmaScript6

---
+++ app.js
@@ -0,0 +1,30 @@
+//Tüm Elementleri Seçme
+const amountElement = document.querySelector("#amount");
+const firstSelect = document.querySelector("#firstCurrency");
+const secondSelect = document.querySelector("#secondCurrency");
+const currency = new Currency("USD","USD");
+const ui = new UI(firstSelect,secondSelect);
+
+//Tüm eventleri çağırıyoruz
+eventListeners();
+
+//Eventleri Çağırma Fonksiyonu
+function eventListeners(){
+    amountElement.addEventListener("input",exchangeCurrency);
+    firstSelect.onchange = function(){
+        currency.changeFirstCurrency(firstSelect.options[firstSelect.selectedIndex].textContent);
+        ui.changeFirst();
+    };
+    secondSelect.onchange = function(){
+        currency.changeSecondCurrency(secondSelect.options[secondSelect.selectedIndex].textContent);
+        ui.changeSecond(); 
+    };
+}
+function exchangeCurrency(){
+    currency.changeAmount(amountElement.value);
+    currency.exchange()
+    .then(result => {
+        ui.displayResult(result);
+    })
+    .catch(err => console.log(err));
+}
